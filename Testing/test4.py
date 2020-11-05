ac7f9c7bc4aa8cc5c6514d4169afd7ad1b1faa02
Tricky <tricky@beon.ca>
2020-11-04 21:25:07 -0500

    Can write some text and do something else

--- Testing/test4.py
+++ Testing/test4.py
@@ -31,12 +31,45 @@ def my_copy(master_fd, master_read=pty._read, stdin_read=pty._read):
             time.sleep(1)
             os.write(master_fd, data)
             time.sleep(1)
+            data_2 = list("This is a test!")
+            for char in data_2:
+                os.write(master_fd, char.encode())
+            time.sleep(1)
 #        if STDIN_FILENO in rfds:
 #            data = stdin_read(STDIN_FILENO)
 #            if not data:
 #                fds.remove(STDIN_FILENO)
 #            else:
 #                pty._writen(master_fd, data)
+
+def my_copy_2(master_fd, master_read=pty._read, stdin_read=pty._read):
+    """Parent copy loop.
+    Copies
+            pty master -> standard output   (master_read)
+            standard input -> pty master    (stdin_read)"""
+    fds = [master_fd, STDIN_FILENO]
+    my_text = ["i"]
+    my_text.append(list("Hi, this a test!\n"))
+    i = 0
+    while True:
+        rfds, wfds, xfds = select(fds, [], [])
+        if master_fd in rfds:
+            data = master_read(master_fd)
+            if not data:  # Reached EOF.
+                fds.remove(master_fd)
+            else:
+                os.write(STDOUT_FILENO, data)
+
+        elif STDIN_FILENO in rfds:
+            try:
+                data = my_text[i].encode()
+                time.sleep(1)
+                os.write(master_fd, data)
+                time.sleep(1)
+            except IndexError:
+                break
+        i += 1
+
 
 def my_spawn(argv, master_read=pty._read, stdin_read=pty._read):
     """Create a spawned process."""
