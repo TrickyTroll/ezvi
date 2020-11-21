21c588d2b4189940b513ce80d4e362827af956a3
Tricky Troll <tricky@beon.ca>
2020-11-21 18:13:47 -0500

    Added `ezvi.py` and started documenting `ez_write()`.

---
+++ Testing/ezvi.py
@@ -0,0 +1,7 @@
+from funcmodule import *
+
+# Need to remove this later. This is what the instructions should look like.
+writing = {"insert":"i", "text":"foooo", "newline":"\n", "text":"bar", "escape":chr(27)}
+
+ez_spawn("vi")
+print("Done")
